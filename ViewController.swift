6f88be88de9f669188d6cbb43f65ae17f1c2bbe7
jiwoo <118118326+jiwoo0413@users.noreply.github.com>
2023-05-20 01:03:58 +0900

    MapSafari
    모비우스 서버에 올려진 cin을 값을 가져와서 사파리에서 열기

---
+++ ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by 홍지우 on 2023/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var uploadButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        uploadButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+//        yourButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+//        uploadButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+
+//    func postLocation() {
+//        let parameters = "{\n    \"m2m:cin\": {\n        \"con\": \"36.769794975534516,126.9323220669489\"\n    }\n}"
+//        let postData = parameters.data(using: .utf8)
+//
+//        var request = URLRequest(url: URL(string: "http://203.253.128.177:7579/Mobius/zz/Map")!,timeoutInterval: Double.infinity)
+//        request.addValue("application/json", forHTTPHeaderField: "Accept")
+//        request.addValue("12345", forHTTPHeaderField: "X-M2M-RI")
+//        request.addValue("Sx2AhUKtTeH", forHTTPHeaderField: "X-M2M-Origin")
+//        request.addValue("application/vnd.onem2m-res+json; ty=4", forHTTPHeaderField: "Content-Type")
+//
+//        request.httpMethod = "POST"
+//        request.httpBody = postData
+//
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//          guard let data = data else {
+//            print(String(describing: error))
+//            return
+//          }
+//          print(String(data: data, encoding: .utf8)!)
+//        }
+//
+//        task.resume()
+//
+//    }
+//
+//    @objc func buttonTapped() {
+//        postLocation()
+//    }
+
+    @objc func buttonTapped() {
+        fetchCinValueFromMobiusServer()
+    }
+    
+    func fetchCinValueFromMobiusServer() {
+        let url = URL(string: "http://203.253.128.177:7579/Mobius/zz/Map")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+                DispatchQueue.main.async {
+                    self.uploadButton.tintColor = .red
+                }
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                DispatchQueue.main.async {
+                    self.uploadButton.tintColor = .red
+                }
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let cinValue = (json as? [String: Any])?["cin"] as? String {
+                    print("CIN value: \(cinValue)")
+                    DispatchQueue.main.async {
+                        self.openSafari(with: cinValue)
+                    }
+                } else {
+                    print("Invalid JSON format or missing 'cin' key")
+                    DispatchQueue.main.async {
+                        self.uploadButton.tintColor = .red
+                    }
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+                DispatchQueue.main.async {
+                    self.uploadButton.tintColor = .red
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func openSafari(with cinValue: String) {
+        guard let safariURL = URL(string: "https://www.example.com/(cinValue)") else {
+            print("Invalid URL")
+            DispatchQueue.main.async {
+                self.uploadButton.tintColor = .red
+            }
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(safariURL) {
+            UIApplication.shared.open(safariURL)
+        } else {
+            print("Unable to open Safari")
+            DispatchQueue.main.async {
+                self.uploadButton.tintColor = .red
+            }
+        }
+    }
+
+}
+
